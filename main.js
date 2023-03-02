980df837b56ed74acce4c65c99e6a74b3125a171
VishiBlaster <mongajatin2588@gmail.com>
2023-03-02 18:33:01 +0530

    many changes

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-document.getElementById("demo").innerHTML="Hello JavaScript";
-console.log("Hello World!")
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+  }
+function myFunction2() {
+    document.getElementById("demo").innerHTML = "Paragraph not changed.";
+  }
+function something(params) {
+    document.write(5+6)
+}
+function bla() {
+    document.save()
+}
