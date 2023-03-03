31c7da7f52ff253c405c23cccfdf641d6dc246a5
VishiBlaster <mongajatin2588@gmail.com>
2023-03-03 21:10:57 +0530

    oops

--- main.js
+++ main.js
@@ -10,10 +10,33 @@ function something(params) {
 function bla() {
     document.save()
 }
-let fahrenheit = document.getElementById("cel")
+/*
+var fahrenheit = document.getElementById("text").value;
+function somethingelse(fahrenheit) {
+    document.getElementById("result").innerHTML = fahrenheit
+}
 function celsiuss(params) {
     return (5/9) * (fahrenheit-32);
 }
-function result(celsiuss) {
+*/
+
+
+car = {
+    owner_first_name : "baa",
+    owner_last_name : "sheep",
+    owner_full_name : function full_name(owner_first_name, owner_last_name) {
+        return this.owner_full_name = owner_first_name + owner_last_name;
+        },
+    car_brand : "lamborghini",
+    car_model : "hurricane"
+}
+    
+
+console.log(this.car.car_model);
+function car2(car) {
+    
+}
+/*function result(celsiuss) {
     document.getElementById("result").innerHTML = "Your result is "+celsiuss;
 }
+*/
